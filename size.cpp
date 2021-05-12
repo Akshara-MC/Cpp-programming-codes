587af7a244cc16c5704d59f632c99cacf3e438ce
Akshara-MC <70649742+Akshara-MC@users.noreply.github.com>
2021-05-12 12:47:42 +0530

    datatypesize.cpp
    The output will be :
    Size of char:1 byte
    Size of int:4 bytes
    Size of float:4 bytes
    Size of double:8 bytes

---
+++ size.cpp
@@ -0,0 +1,12 @@
+//Program to find size of int,char,float,double
+#include<iostream>
+using namespace std;
+int main()
+{
+	std::cout<<"Size of char:"<<sizeof(char)<<"byte"<<"\n";
+	std::cout<<"Size of int:"<<sizeof(int)<<"bytes"<<"\n";
+	std::cout<<"Size of float:"<<sizeof(float)<<"bytes"<<"\n";
+	std::cout<<"Size of double:"<<sizeof(double)<<"bytes"<<"\n";
+	return 0;
+	
+}
