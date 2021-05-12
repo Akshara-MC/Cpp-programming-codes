993594b4b382cba08ec0461442ac623a70573f54
Akshara-MC <70649742+Akshara-MC@users.noreply.github.com>
2021-05-12 12:52:19 +0530

    product_of_2_numbers.cpp
    The output will be:
    Enter any two numbers:26  5
    Product of 2 numbers is:130

---
+++ product.cpp
@@ -0,0 +1,12 @@
+//Program to find the product of 2 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,pro;
+  std::cout<<"Enter any two numbers:";
+  std::cin>>a>>b;
+  pro=a*b;
+  std::cout<<"Product of 2 numbers is:"<<pro<<endl;
+  return 0;	
+}
